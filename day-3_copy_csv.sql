8f99b74d06237c8a35bab126d4fb980a84d7068d
akshaybende <akshaybende0@gmail.com>
2025-08-22 20:21:12 +0530

    Add files via upload

---
+++ day-3_copy_csv.sql
@@ -0,0 +1,28 @@
+select * from cmp
+
+delete  from cmp
+
+select * from cmp
+
+
+D:/da20/sql/day-3/2_july_2025_cmp_elec_data.csv
+
+copy cmp from 'D:/da20/sql/day-3/2_july_2025_cmp_elec_data.csv' DELIMITER ',' csv header
+
+
+select * from cmp
+
+
+copy cmp from 'D:/da20/sql/day-3/2_july_2025_cmp_elec_data.csv' DELIMITER ',' csv header
+
+
+select * from cmp
+
+copy cmp from 'D:/da20/sql/day-3/2_july_2025_cmp_elec_data.csv' DELIMITER ',' csv header
+
+
+
+select * from cmp
+
+
+
